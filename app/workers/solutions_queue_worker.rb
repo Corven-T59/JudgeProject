92a273140f9073e7648d315573e69ac0432b4c50
Aldo Rincón Mora <aldo.rinconmora@hotmail.com>
2017-03-02 17:22:11 -0500

    App worker

--- app/workers/solutions_queue_worker.rb
+++ app/workers/solutions_queue_worker.rb
@@ -1,7 +1,33 @@
+require "open3"
+
 class SolutionsQueueWorker
   include Sidekiq::Worker
 
-  def perform(*args)
-    # Do something
+  def perform(solution_id)
+    begin
+      #solution = Solution.includes(:problem).find(solution_id)
+      #problem = solution.problem
+
+      project_directory = %x(pwd).chomp
+      run_sh = File.join(project_directory,"lib","judge","run.sh")
+      compare_sh = File.join(project_directory,"lib","judge","compare.sh")
+      path_temp = "/tmp/solution_#{solution_id}"
+      cp = %x(which cp) || "/bin/cp"
+      unless system("mkdir #{path_temp}") then
+      	return; #Todo :Raise an error.
+      end
+
+      cmd = "sudo "+run_sh+" source lib/testBoca/source.cpp lib/testBoca/input C++ source 1"
+      puts cmd
+      Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+        puts "Hola mundo\n"
+        wait_thr.join()
+        puts stdout.gets
+        puts wait_thr.value.exitstatus
+      end
+
+      system("rm -r #{path_temp}")
+
+    end
   end
 end
