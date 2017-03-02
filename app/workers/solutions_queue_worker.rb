1478e18a035129af52129015b8af37339a41aa8c
Aldo Rincón Mora <aldo.rinconmora@hotmail.com>
2017-03-02 18:21:14 -0500

    Added :3

--- app/workers/solutions_queue_worker.rb
+++ app/workers/solutions_queue_worker.rb
@@ -1,32 +1,50 @@
 require "open3"
 
+
+# the return code show what happened:
+# 0 ok
+# 1 compile error
+# 2 runtime error
+# 3 timelimit exceeded
+# other_codes are unknown to boca: in this case BOCA will present the
+#                                  last line of standard output to the judge
 class SolutionsQueueWorker
   include Sidekiq::Worker
 
   def perform(solution_id)
     begin
-      #solution = Solution.includes(:problem).find(solution_id)
-      #problem = solution.problem
+      solution = Solution.includes(:problem).find(solution_id)
+      problem = solution.problem
 
       project_directory = %x(pwd).chomp
       run_sh = File.join(project_directory,"lib","judge","run.sh")
       compare_sh = File.join(project_directory,"lib","judge","compare.sh")
       path_temp = "/tmp/solution_#{solution_id}"
       cp = %x(which cp) || "/bin/cp"
-      unless system("mkdir #{path_temp}") then
-      	return; #Todo :Raise an error.
-      end
+      basename = problem.baseName
+      source_code = File.join(project_directory,"public",solution.solutionFile.url)
+      input = File.join(project_directory,"public",problem.inputFile.url)
+      name = problem.name
+      time_limit = problem.timeLimit
 
-      cmd = "sudo "+run_sh+" source lib/testBoca/source.cpp lib/testBoca/input C++ source 1"
+      # parameters are:
+      # $1 base_filename
+      # $2 source_file
+      # $3 input_file
+      # $4 languagename
+      # $5 problemname
+      # $6 timelimit
+
+      cmd = "sudo #{run_sh} #{basename} #{source_code} #{input} C++ '#{name}' #{time_limit}"
       puts cmd
       Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-        puts "Hola mundo\n"
-        wait_thr.join()
+        puts "\n\n\n stdout:\n"
         puts stdout.gets
-        puts wait_thr.value.exitstatus
+        puts "\n\n\n stderr:\n"
+        puts stderr.gets
+        puts "\n\n\n exit:\n"
+        puts wait_thr.value
       end
-
-      system("rm -r #{path_temp}")
 
     end
   end
---
+++ lib/testBoca/runtimeError.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+int a = 0/0;
+    printf("1\n");
+    return 0;
+}
