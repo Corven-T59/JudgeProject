fe9e1e690a0bd0b00ebd174dd8455b65689b7c55
CorvenT59 <dandresto@hotmail.com>
2017-03-25 09:54:17 -0500

    Added administrator permissions manager

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,15 @@ class ApplicationController < ActionController::Base
   protected
     #check if the user is an administrator
     def is_admin
-    	if !current_user.admin
-    		redirect_to root_path, :alert => "Access denied"
-      end
+    	if current_user != nil
+	  	  	if !current_user.admin
+	    		redirect_to root_path, :alert => "Access denied"
+	      	end
+		  else
+					redirect_to root_path, :alert => "Access denied"
+		  end
+  		
+
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
-  
+
   authenticate :user do
+
     resources :contests, except: [:index, :show] do
       post 'subscribe', on: :member
       post 'unsubscribe', on: :member
@@ -20,6 +21,7 @@ Rails.application.routes.draw do
   resources :executions, only: [:show, :index]
 
   devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
