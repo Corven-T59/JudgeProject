bc87a4b72d1f35a1eae6eec9c116d73a36194df7
Aldo Rincón Mora <aldo.rinconmora@hotmail.com>
2017-04-15 14:26:10 -0500

    Remove execution model from testing
    Signed-off-by: Aldo Rincón Mora <aldo.rinconmora@hotmail.com>

--- spec/judge/answers_spec.rb
+++ spec/judge/answers_spec.rb
@@ -1,43 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Solution, type: :model do
-  #["cpp", "java", "ruby", "python", "python3", "csharp"].each do |lang|
-    ["ruby", "csharp"].each do |lang|
+  ["cpp", "java", "ruby", "python", "python3"].each do |lang|
+    #  ["ruby", "csharp"].each do |lang|
       describe "Programming languages" do
       context lang.capitalize do
         it "Creates a execution result" do
           Sidekiq::Testing.inline!
           expect{
             FactoryGirl.create(:solution)
-          }.to change(Execution,:count).by(1)
+          }.to change(Solution, :count).by(1)
         end
 
         it "Creates a execution result: #{lang} OK" do
           Sidekiq::Testing.inline!
           @ok = FactoryGirl.create("#{lang}_ok".to_sym)
-          expect(Execution.last.status.to_i).to eq(4)
+          expect(Solution.last.status.to_i).to eq(4)
         end
         it "Creates a execution result: #{lang} WA" do
           Sidekiq::Testing.inline!
           @wa = FactoryGirl.create("#{lang}_wa".to_sym)
-          expect(Execution.last.status.to_i>5).to be_truthy
+          expect(Solution.last.status.to_i>5).to be_truthy
         end
         unless ["ruby", "python", "python3"].include?(lang) then
           it "Creates a execution result: #{lang} CE" do
             Sidekiq::Testing.inline!
             @ce = FactoryGirl.create("#{lang}_ce".to_sym)
-            expect(Execution.last.status.to_i).to eq(1)
+            expect(Solution.last.status.to_i).to eq(1)
           end
         end
         it "Creates a execution result: #{lang} RTE" do
           Sidekiq::Testing.inline!
           @rte = FactoryGirl.create("#{lang}_rte".to_sym)
-          expect(Execution.last.status.to_i).to eq(2)
+          expect(Solution.last.status.to_i).to eq(2)
         end
         it "Creates a execution result: #{lang} TLE" do
           Sidekiq::Testing.inline!
           @tle = FactoryGirl.create("#{lang}_tle".to_sym)
-          expect(Execution.last.status.to_i).to eq(3)
+          expect(Solution.last.status.to_i).to eq(3)
         end
       end
     end
