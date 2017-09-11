425449bd5624e838bd376a879c189a384ecb84bb
Aldo Rincón Mora <aldo.rinconmora@hotmail.com>
2017-09-11 21:28:07 +0200

    Revert "Disable web notifications"
    This reverts commit fdd754d
    
    Signed-off-by: Aldo Rincón Mora <aldo.rinconmora@hotmail.com>

---
+++ app/assets/javascripts/channels/web_notifications.js
@@ -0,0 +1,26 @@
+customConnect = function () {
+    App.cable.connect();
+};
+
+App.web_notifications = App.cable.subscriptions.create("WebNotificationsChannel", {
+    connected: function () {
+        // Called when the subscription is ready for use on the server
+        console.log("connected");
+    },
+
+    disconnected: function () {
+        // Called when the subscription has been terminated by the server
+        console.log("disconected");
+    },
+    received: function (data) {
+        console.log(data);
+        var options = {
+            content: data["message"], // text of the snackbar
+            style: "snackbar", // add a custom class to your snackbar
+            timeout: 8000 // time in milliseconds after the snackbar autohides, 0 is disabled
+        };
+        $.snackbar(options);
+        location.reload();
+    }
+
+});
